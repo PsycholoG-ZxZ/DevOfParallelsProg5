26b242f0f9509305b5a6afc37583ddd78f3c4ed7
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-27 00:37:16 +0300

    Scripted auto-commit on change (2019-12-27 00:37:16) by gitwatch.sh

--- 11 Lab5/src/main/java/FlowTreatment.java
+++ 11 Lab5/src/main/java/FlowTreatment.java
@@ -1,11 +1,14 @@
 import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
 public class FlowTreatment {
 
-    public Flow<HttpRequest, HttpResponse, NotUsed> FlowTreatment(){
+    public Flow<HttpRequest, HttpResponse, NotUsed> FlowTreatment(Http, ActorSystem, ActorMaterializer){
 
 
     }
