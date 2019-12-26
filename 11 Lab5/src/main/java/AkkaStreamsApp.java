0877ffa94362fc4d14b4b77e03ac47cc4d11cdd2
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-26 21:05:28 +0300

    Scripted auto-commit on change (2019-12-26 21:05:28) by gitwatch.sh

--- 11 Lab5/src/main/java/AkkaStreamsApp.java
+++ 11 Lab5/src/main/java/AkkaStreamsApp.java
@@ -1,8 +1,21 @@
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import jdk.internal.org.objectweb.asm.Handle;
+
 import java.io.IOException;
 
 public class AkkaStreamsApp {
     public static void main(String[] args) throws IOException {
-        System.out.println();
-        
+        System.out.println("start!");
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = <HttpCatcher(http>
+
     }
 }
