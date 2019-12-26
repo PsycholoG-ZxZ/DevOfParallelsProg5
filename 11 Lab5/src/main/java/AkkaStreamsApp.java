82c08148e57d40b0b9a5c27615f722b4a1e35997
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-26 21:09:13 +0300

    Scripted auto-commit on change (2019-12-26 21:09:13) by gitwatch.sh

--- 11 Lab5/src/main/java/AkkaStreamsApp.java
+++ 11 Lab5/src/main/java/AkkaStreamsApp.java
@@ -1,6 +1,8 @@
 import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
@@ -8,6 +10,7 @@ import akka.stream.javadsl.Flow;
 import jdk.internal.org.objectweb.asm.Handle;
 
 import java.io.IOException;
+import java.util.concurrent.CompletionStage;
 
 public class AkkaStreamsApp {
     public static void main(String[] args) throws IOException {
@@ -15,7 +18,17 @@ public class AkkaStreamsApp {
         ActorSystem system = ActorSystem.create("routes");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = <HttpCatcher(http>
-
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = ;
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+                );
+        System.out.println("server Online at localhost");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate()); // OFF when done
     }
+    
 }
