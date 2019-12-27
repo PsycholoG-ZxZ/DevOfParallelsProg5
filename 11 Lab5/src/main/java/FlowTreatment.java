1a29c45a5e5635e1df18db267073496f9869126c
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-27 15:16:43 +0300

    Scripted auto-commit on change (2019-12-27 15:16:43) by gitwatch.sh

--- 11 Lab5/src/main/java/FlowTreatment.java
+++ 11 Lab5/src/main/java/FlowTreatment.java
@@ -70,7 +70,7 @@ public class FlowTreatment {
                 .mapAsync(4, f ->{
                     Long Begin = System.currentTimeMillis();
                     AsyncHttpClient asyncHttpClient = asyncHttpClient();
-                    return  asyncHttpClient.prepareGet
+                    return  asyncHttpClient.prepareGet(pair)
                 })
 
     }
