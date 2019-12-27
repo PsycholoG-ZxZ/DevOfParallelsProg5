92d30f97c609035f5c45306dc1573ba68e92adc8
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-27 15:36:15 +0300

    Scripted auto-commit on change (2019-12-27 15:36:15) by gitwatch.sh

--- 11 Lab5/src/main/java/AkkaStreamsApp.java
+++ 11 Lab5/src/main/java/AkkaStreamsApp.java
@@ -22,7 +22,7 @@ public class AkkaStreamsApp {
 
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        FlowTreatment
+        final FlowTreatment
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = new FlowTreatment(http, system, materializer);
 
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
