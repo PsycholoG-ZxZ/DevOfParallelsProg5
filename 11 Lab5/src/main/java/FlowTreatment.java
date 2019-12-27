422572c47a45762cec2a1c8258d60e7e274fa154
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-27 14:46:13 +0300

    Scripted auto-commit on change (2019-12-27 14:46:13) by gitwatch.sh

--- 11 Lab5/src/main/java/FlowTreatment.java
+++ 11 Lab5/src/main/java/FlowTreatment.java
@@ -30,7 +30,7 @@ public class FlowTreatment {
                 .mapAsync(4, f -> Patterns.ask(storeActor, f, Duration.ofMillis(5000))
                         .thenCompose(ms -> {
                             ResponseResult response = (ResponseResult) ms;
-                            
+                            if (response.getFlag_about_contains() == 1)
 
                         })
                 )
