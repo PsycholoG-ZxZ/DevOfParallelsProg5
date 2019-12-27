244dd2e6de7131b62f4dd6dc62a880022b34883c
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-27 08:34:34 +0300

    Scripted auto-commit on change (2019-12-27 08:34:34) by gitwatch.sh

--- 11 Lab5/src/main/java/StorageActor.java
+++ 11 Lab5/src/main/java/StorageActor.java
@@ -1,5 +1,6 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class StorageActor {
-    private Map<UrlCountInfo, String> storage = 
+    private Map<UrlCountInfo, String> storage = new HashMap<>();
 }
