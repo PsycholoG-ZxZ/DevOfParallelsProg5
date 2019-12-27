8f8b7799052433d5052fa7c749ddff7f49f5c0ba
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-27 15:35:09 +0300

    Scripted auto-commit on change (2019-12-27 15:35:09) by gitwatch.sh

--- 11 Lab5/src/main/java/UrlCountInfo.java
+++ 11 Lab5/src/main/java/UrlCountInfo.java
@@ -3,9 +3,14 @@ import java.util.Optional;
 public class UrlCountInfo {
 
     private Optional<String> link, count;
+    private String cnt;
 
     public UrlCountInfo(Optional<String> link, Optional<String> count){
         this.count = count;
+        this.link = link;
+    }
+    public UrlCountInfo(Optional<String> link, String count){
+        this.cnt = count;
         this.link = link;
     }
     public Optional<String> getLink(){
