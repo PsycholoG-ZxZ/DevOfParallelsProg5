40d3a185406195fa7b10a6911f7158ef00afa3e4
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-27 14:45:36 +0300

    Scripted auto-commit on change (2019-12-27 14:45:36) by gitwatch.sh

--- 11 Lab5/src/main/java/ResponseResult.java
+++ 11 Lab5/src/main/java/ResponseResult.java
@@ -5,6 +5,21 @@ public class ResponseResult {
     private Long time;
     private int flag_about_contains; // 0 or 1
 
-    public ResponseResult(int flag, )
+    public ResponseResult(int flag, Optional<String> link, Long time){
+        this.flag_about_contains = flag;
+        this.link = link;
+        this.time = time;
+    }
 
+    public int getFlag_about_contains() {
+        return flag_about_contains;
+    }
+
+    public Long getTime() {
+        return time;
+    }
+
+    public Optional<String> getLink() {
+        return link;
+    }
 }
