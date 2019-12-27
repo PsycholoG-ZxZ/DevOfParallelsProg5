4730b3dc121015e157084597fe95825ada1bc32b
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-27 14:19:55 +0300

    Scripted auto-commit on change (2019-12-27 14:19:55) by gitwatch.sh

--- 11 Lab5/src/main/java/StorageActor.java
+++ 11 Lab5/src/main/java/StorageActor.java
@@ -14,8 +14,6 @@ public class StorageActor {
                     Long time = f.getTime();
                     storage.put(f.getTest(),time.toString());
                 })
-                .match(UrlCountInfo.class, f -> {
-                    
-                })
+                .match(UrlCountInfo.class, f -> sender.)
     }
 }
