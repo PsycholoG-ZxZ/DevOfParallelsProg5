5f399ef4617d8a27f4e9301274fa1dfb3a599f1a
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-27 14:38:14 +0300

    Scripted auto-commit on change (2019-12-27 14:38:14) by gitwatch.sh

--- 11 Lab5/src/main/java/FlowTreatment.java
+++ 11 Lab5/src/main/java/FlowTreatment.java
@@ -15,7 +15,7 @@ public class FlowTreatment {
     private Http http;
     private ActorSystem system;
     private ActorMaterializer materializer;
-    ActorRef 
+    ActorRef storeActor = 
 
     public Flow<HttpRequest, HttpResponse, NotUsed> FlowTreatment(Http http, ActorSystem actorSys, ActorMaterializer mater){
         this.http = http;
