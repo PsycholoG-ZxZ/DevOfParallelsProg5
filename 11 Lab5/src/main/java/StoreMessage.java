34079635defac75818dfdf9a36f2bc30f8f10611
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-27 14:13:36 +0300

    Scripted auto-commit on change (2019-12-27 14:13:36) by gitwatch.sh

--- 11 Lab5/src/main/java/StoreMessage.java
+++ 11 Lab5/src/main/java/StoreMessage.java
@@ -1,3 +1,17 @@
+import java.util.Optional;
+
 public class StoreMessage {
-    
+    private Optional<String> link, count;
+
+    public UrlCountInfo(Optional<String> link, Optional<String> count){
+        this.count = count;
+        this.link = link;
+    }
+    public Optional<String> getLink(){
+        return link;
+    }
+
+    public Optional<String> getCount() {
+        return count;
+    }
 }
