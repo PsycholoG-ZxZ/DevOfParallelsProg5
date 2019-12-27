bad3290207eb89103938b16acca92d1510f5af54
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-27 08:36:55 +0300

    Scripted auto-commit on change (2019-12-27 08:36:55) by gitwatch.sh

--- 11 Lab5/src/main/java/StorageActor.java
+++ 11 Lab5/src/main/java/StorageActor.java
@@ -10,6 +10,6 @@ public class StorageActor {
     @Override
     public AbstractActor.Receive createReceive(){
         return ReceiveBuilder.create()
-                .match(UrlCountInfo.class)
+                .match(UrlCountInfo.class, )
     }
 }
