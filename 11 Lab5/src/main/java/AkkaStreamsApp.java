54daeb668894ceed99bedf13450e8babfc13801d
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-27 08:31:14 +0300

    Scripted auto-commit on change (2019-12-27 08:31:14) by gitwatch.sh

--- 11 Lab5/src/main/java/AkkaStreamsApp.java
+++ 11 Lab5/src/main/java/AkkaStreamsApp.java
@@ -16,7 +16,7 @@ public class AkkaStreamsApp {
     public static void main(String[] args) throws IOException {
         System.out.println("start!");
         ActorSystem system = ActorSystem.create("routes");
-        ActorREf
+        ActorRef store
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = FlowTreatment(http, system, materializer);
