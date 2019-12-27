ec1b46c980d18983677707077301207eba51081b
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-12-27 14:57:23 +0300

    Scripted auto-commit on change (2019-12-27 14:57:23) by gitwatch.sh

--- 11 Lab5/src/main/java/FlowTreatment.java
+++ 11 Lab5/src/main/java/FlowTreatment.java
@@ -38,7 +38,7 @@ public class FlowTreatment {
                                 return CompletableFuture.completedFuture(response);
                             }
                             return Source.from(Collections.singletonList(f))
-                                    .toMat()
+                                    .toMat((te))
 
                         })
                 )
